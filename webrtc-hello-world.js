86767473ae3675a5d82fbc90c3a63f78d4feb1d1
Vamsi Krishna Sure <vamsisk.git@outlook.com>
2017-01-16 21:09:03 +0000

    Running HTTPS server with self signed certificates.

--- webrtc-hello-world.js
+++ webrtc-hello-world.js
@@ -1,12 +1,16 @@
-var http = require('http');
+var https = require('https');
 var fs = require('fs');
 
+const options = {
+    key: fs.readFileSync('./key.pem'),
+    cert: fs.readyFileSync('./cert.pem')
+};
 
 fs.readFile('./pages/webrtc-hello-world.html', function (err, html) {
     if (err) {
         throw err;
     }
-    http.createServer(function (request, response) {
+    https.createServer(options, function (request, response) {
         response.writeHeader(200, {"Content-Type": "text/html"});
         response.write(html);
         response.end();
